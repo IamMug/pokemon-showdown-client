ef1321dd8a015c7cbfc5b98795e759350f43ea94
IamMug <108377633+IamMug@users.noreply.github.com>
2024-11-06 18:44:03 -0330

    Please

--- play.pokemonshowdown.com/config/config.js
+++ play.pokemonshowdown.com/config/config.js
@@ -19,7 +19,7 @@ Config.whitelist = [
 // address bar is `Config.routes.client`.
 Config.defaultserver = {
 	id: 'showdown',
-	host: 'sim3.psim.us',
+	host: '51.81.33.196',
 	port: 443,
 	httpport: 8000,
 	altport: 80,
